644ee196ef0f2183f3108f47e97a476303934d9c
Gislaine Amancio <gimcjf@gmail.com>
2021-09-24 14:22:28 -0300

    alterações no exercio sobre salario

--- src/ex02.java
+++ src/ex02.java
@@ -7,12 +7,24 @@ public class ex02 {
 
         System.out.println("Digite o valor do salario: ");
         double salario = Double.parseDouble(scanner.nextLine());
+        
 
-        if(salario > 1997.58){
-            System.out.println("Salario maior que R$997.58");
-        }else if(salario > 5399.99){
-        }else if(salario >= 0){
-            System.out.println("Salario maior ou igual a zero");
+        if(salario > 1997.58  && salario < 5399.99){
+            System.out.println("Salario maior que R$1997.58");
+        }else if(salario >= 5399.99){
+            System.out.println("Salario maior que R$5399.99");
+        }else if(salario <= 0){
+            System.out.println("Salario menor ou igual a zero: ");
+            do{
+                System.out.println("Salario precisa estar entre 1997.58 e 5399.99");
+                if(salario <= 0){
+                    System.out.println("Digite salario novamente: ");
+                    salario = Double.parseDouble(scanner.nextLine());
+                    
+
+                }                
+               
+            }while(salario <= 0);
 
         }
     }
