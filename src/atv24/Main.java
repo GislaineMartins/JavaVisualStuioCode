d3c2fc3c0e9cac73e6218d67a8ee061fc5222fb4
Gislaine Amancio <gimcjf@gmail.com>
2021-10-18 14:13:00 -0300

    Criando objetos

--- src/atv24/Main.java
+++ src/atv24/Main.java
@@ -3,7 +3,16 @@ package atv24;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Veiculo veiculo = new Veiculo();
+        veiculo.marca = "Fiat";
+        veiculo.modelo = "Fiat Uno";
+        veiculo.placa = "QRT1234";
+
+        Carro carro = new Carro();
+        carro.numPortas = 4;
+        carro.capciPortaMalas = 1000.00;
+
     }
     
 }
