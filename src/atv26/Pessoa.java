e3e42a727111b38dcfc0e562f747a03e1b136c3e
Gislaine Amancio <gimcjf@gmail.com>
2021-10-19 14:35:24 -0300

    Implementação do metodo equals na classe Pessoa

--- src/atv26/Pessoa.java
+++ src/atv26/Pessoa.java
@@ -6,6 +6,19 @@ public class Pessoa {
     public String sobrenome;
     public int idade;
 
+    
+    @Override
+    public boolean equals(Object obj) {
+        Pessoa pessoa = (Pessoa)obj;
+        if(pessoa.nome == this.nome && pessoa.sobrenome == this.sobrenome && pessoa.idade == this.idade){
+            return true;
+        }
+        return false;
+       
+    }
+
+
+
     @Override
     public String toString() {
         String msg = "Nome: " + this.nome
