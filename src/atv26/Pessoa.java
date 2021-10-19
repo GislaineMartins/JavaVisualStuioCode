07a411cf4df2da6601e19b7e7a46e08525391895
Gislaine Amancio <gimcjf@gmail.com>
2021-10-19 14:23:50 -0300

    Implementação do metodo toString na classe Pessoa

--- src/atv26/Pessoa.java
+++ src/atv26/Pessoa.java
@@ -5,5 +5,13 @@ public class Pessoa {
     public String nome;
     public String sobrenome;
     public int idade;
+
+    @Override
+    public String toString() {
+        String msg = "Nome: " + this.nome
+        + "Sobrenome: " + this.sobrenome
+        + "Idade: " + this.idade;
+        return msg;
+    }
     
 }
