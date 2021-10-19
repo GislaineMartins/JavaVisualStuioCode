9174e6b2b394c050c6bfd0662614f4810c27f52a
Gislaine Amancio <gimcjf@gmail.com>
2021-10-19 14:23:10 -0300

    Implementação do metodo toString na classe Aluno

--- src/atv26/Aluno.java
+++ src/atv26/Aluno.java
@@ -5,5 +5,13 @@ public class Aluno extends Pessoa{
     public int matricula;
     public String turma;
     public String curso;
+
+    @Override
+    public String toString() {
+        String msg =  super.toString() + "Matricula: " + this.matricula 
+        + "Turma: " + this.turma
+        + "Curso: " + this.curso;
+        return msg;
+    }
     
 }
