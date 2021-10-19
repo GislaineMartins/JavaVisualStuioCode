99caab25c86aee8b7d2985e64d47dd009c4acaff
Gislaine Amancio <gimcjf@gmail.com>
2021-10-19 14:34:41 -0300

    Implementaçaõ do metodo equals na classe Aluno

--- src/atv26/Aluno.java
+++ src/atv26/Aluno.java
@@ -6,6 +6,18 @@ public class Aluno extends Pessoa{
     public String turma;
     public String curso;
 
+   
+    @Override
+    public boolean equals(Object obj) {
+        Aluno aluno = (Aluno)obj;
+        if(aluno.matricula == this.matricula && aluno.turma == this.turma && aluno.curso == this.curso){
+            return true;
+        }
+        return false;
+    }
+
+
+
     @Override
     public String toString() {
         String msg =  super.toString() + "Matricula: " + this.matricula 
